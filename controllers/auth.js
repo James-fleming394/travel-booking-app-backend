c2cd75ae96f5b6e013535f3a3a4d01150db8ce45
james-fleming394 <james.fleming394@gmail.com>
2022-12-19 17:52:02 -0500

    Progress on Auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,19 @@
 const User = require('../models/User.js');
+var bcrypt = require('bcryptjs');
+
+
+//REGISTER 
 
 const register = async (req, res) => {
     try {
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hash
         })
 
         await newUser.save();
@@ -13,8 +21,27 @@ const register = async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
+};
+
+//LOGIN
+
+const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+        if (!user) return next(createError(404, "User not found!"));
+
+        const isRightPassword = await bcrypt.compare(req.body.password, user.password)
+        if (isRightPassword) return next(createError(400, "Wrong password or username!"));
+
+        const {password, isAdmin, ...otherDetails} = user._doc;
+
+        res.status(200).json({...otherDetails});
+    } catch (err) {
+        res.status(500).json(err);
+    }
 }
 
 module.exports = {
-    register
+    register,
+    login
 }
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const controlAuth = require('../controllers/auth.js');
 const router = express.Router();
 
 router.post("/register", controlAuth.register);
+router.post("/login", controlAuth.login);
 
 
 module.exports = router;

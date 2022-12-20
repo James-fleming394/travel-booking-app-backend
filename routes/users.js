74ae4fba62fce08d724bd2a3ee0a1df118dff4bc
james-fleming394 <james.fleming394@gmail.com>
2022-12-20 09:45:12 -0500

    User routes and controllers connect

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const userControls = require('../controllers/user.js');
+const { verifyAdmin, verifyToken, verifyUser } = require('../utility/verifyToken.js');
+
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("User Endpoint")
-})
+//UPDATE
+router.put("/:id", verifyUser, userControls.updateUser);
+
+//DELETE
+router.delete("/:id", verifyUser, userControls.deleteUser);
+
+//GET
+router.get("/:id", verifyUser, userControls.getUser);
+
+//GET ALL
+router.get("/", verifyAdmin, userControls.getUsers);
 
 module.exports = router;

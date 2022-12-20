d8b1c3fd17e1f1f48750c25bcf3515847ce48b03
james-fleming394 <james.fleming394@gmail.com>
2022-12-20 11:22:32 -0500

    Backend running and functional

--- server.js
+++ server.js
@@ -25,6 +25,17 @@ app.use('/api/user', userRoute);
 app.use('/api/hotels', hotelsRoute);
 app.use('/api/rooms', roomsRoute);
 
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
+
 db.on('error', console.error.bind(console, 'MongoDB Connection Error'));
 
 // app.get('/*', (req, res) => {
